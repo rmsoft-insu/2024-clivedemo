11f46dafe99d3973bf50354bfa25d748b849c7cb
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-03-11 15:44:31 +0900

    로그인이 안되었을 때 처리

---
+++ actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { sessionOptions, SessionData, defaultSession } from "@/lib";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
+export const getSession = async () => {
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+
+  if (!session.isLoggedIn) {
+    session.isLoggedIn = defaultSession.isLoggedIn;
+  }
+
+  return session;
+};
+export const login = async () => {};
+export const logout = async () => {};
---
+++ components/logoutForm.tsx
@@ -0,0 +1,7 @@
+export const LogoutForm = () => {
+  return (
+    <form>
+      <button>logout</button>
+    </form>
+  );
+};
