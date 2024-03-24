4a722940b4a464f02e9dd21281b773aaff1cb64d
iinsue <hinsu@rmsoft.kr>
2024-03-24 11:17:49 +0900

    버튼이랑 폼 설치

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 const LoginPage = () => {
   return (
     <>
-      <div> Login Page</div>
+      <div>로그인 페이지</div>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Welcome to the homepage</h1>
-      <p>This page is visible to everyone!</p>
+      <Link href="/login">로그인</Link>
     </div>
   );
 }
