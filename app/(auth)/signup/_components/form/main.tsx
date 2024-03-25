e3f06ef11d16c81d597fa17854ba6eae5aff6682
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-03-25 14:02:01 +0900

    회원가입 - 폼생성

--- app/(auth)/signup/_components/form/main.tsx
+++ app/(auth)/signup/_components/form/main.tsx
@@ -1,7 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const FormSchema = z.object({
+  userId: z.string(),
+  password: z.string(),
+});
+
 export const SignupMain = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log("submit", data);
+  };
   return (
     <>
-      <div>Sign up Main</div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="w-[300px] flex flex-col gap-4">
+            <FormField
+              name="userId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>아이디</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="mt-4">
+            로그인
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
