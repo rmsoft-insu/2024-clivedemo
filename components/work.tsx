b655839ffd29810bf0fbc06de19767f53a0a14a3
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-04-24 17:14:47 +0900

    Update work.tsx

--- components/work.tsx
+++ components/work.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export const WorkButton = () => {
   const router = useRouter();
   return (
     <>
-      <button
-        onClick={() => router.replace("/work?name=insu&id=shine&code=123code")}
+      <Link
+        href={{
+          pathname: "/work",
+          query: {
+            name: "insu",
+            id: "shine",
+            code: "123code",
+          },
+        }}
       >
         작업하기
-      </button>
+      </Link>
     </>
   );
 };
